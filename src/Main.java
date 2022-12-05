52e8021dcc57ca842056763fb2e0198eebbf18ac
a123 <shchevelev.roman@yandex.ru>
2022-12-05 17:32:03 +0300

    Решено 1-2 задача

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
+        int amountOfDays = 31;
         int[] arr = generateRandomArray();
+        int sum = Arrays.stream(arr).sum();
+        System.out.println("Сумма затрат в месяц составила " + sum);
+        int max = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        int min = max;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println("Минимальная сумма трат составила " + min + " рублей. Максимальная сумма трат за день составила " + max + " рублей.");
 
     }
 
